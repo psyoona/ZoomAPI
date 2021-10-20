b4a30966d2c842c8df52cdd4b595d17a9bbdecba
Seyoon Park <psyoona@naver.com>
2021-10-20 22:25:34 +0900

    First Commit

---
+++ ZoomAPI/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿namespace ZoomAPI.Controllers
+{
+	using Microsoft.IdentityModel.Tokens;
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Linq;
+	using RestSharp;
+	using System;
+	using System.Net;
+	using System.Text;
+	using System.Web.Mvc;
+
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public string CreateZoomMeeting()
+		{
+			string email = "이메일 주소를 입력하세요.";
+			var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+			var now = DateTime.UtcNow;
+			// API Secret
+			var apiSecret = "API Secret을 입력하세요.";
+			byte[] symmetricKey = Encoding.ASCII.GetBytes(apiSecret);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				// API Key
+				Issuer = "API Key를 입력하세요.",
+				Expires = now.AddSeconds(300),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256)
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			var tokenString = tokenHandler.WriteToken(token);
+
+			var client = new RestClient($"https://api.zoom.us/v2/users/{email}/meetings");
+			var request = new RestRequest(Method.POST);
+			request.RequestFormat = DataFormat.Json;
+
+			request.AddJsonBody(new { topic = "Meeting with Zoom", duration = "10", start_time = "2021-10-21T05:00:00", type = "2" });
+
+			request.AddHeader("authorization", $"Bearer {tokenString}");
+			IRestResponse restResponse = client.Execute(request);
+			HttpStatusCode statusCode = restResponse.StatusCode;
+			int numericStatusCode = (int)statusCode;
+			var jObject = JObject.Parse(restResponse.Content);
+
+			return JsonConvert.SerializeObject(new { StartUrl = (string)jObject["start_url"], JoinUrl = (string)jObject["join_url"], Code = Convert.ToString(numericStatusCode) });
+		}
+	}
+}
---
+++ ZoomAPI/Scripts/Index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+	$('#create').click(function (e) {
+		$.ajax({
+			url: 'Home/CreateZoomMeeting',
+			type: 'POST',
+			success: function (response) {
+				if (response) {
+					var parsed = JSON.parse(response);
+
+					$('#host').val(parsed.StartUrl);
+					$('#join').val(parsed.JoinUrl);
+					$('#code').val(parsed.Code);
+				}
+				console.log('Success');
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+		});
+	});
+
+	console.log('hihi');
+});
